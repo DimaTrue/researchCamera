05bd3f442e5767de6d3c8fe21ee6984e5a2f8470
Efimenko Dmitriy <montifick77@gmail.com>
2019-10-21 09:58:30 +0300

    refactoring

--- App.js
+++ App.js
@@ -1,10 +1,5 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
-
-import Camera from './src/components/Camera';
 import AppNav from './src/navigation/AppNavigator';
-import MAinScreen from './src/Screens/MainScreen';
-import MainScreen from './src/Screens/MainScreen';
 
 export default class App extends React.Component {
   constructor(props) {
@@ -13,20 +8,6 @@ export default class App extends React.Component {
   }
 
   render() {
-    return (
-      // <View style={styles.container}>
-      //<MainScreen />
-      <AppNav />
-      // </View>
-    );
+    return <AppNav />;
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
